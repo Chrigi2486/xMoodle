8651f5272231e7da3792818aaa2d3e26c1b72d16
Chrigi2486 <christian@cataldo.co.uk>
2021-01-23 13:22:42 +0100

    Threaded the download, as to not freeze the app when downloading

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ class MoodleApp(QMainWindow):
         uic.loadUi('mainpage.ui', self)
         self.setFixedSize(902, 501)
 
+        self.threadpool = QtCore.QThreadPool()
+
         if not check_for_file('./config.json'):
             self.set_default_settings()
 
@@ -89,97 +91,35 @@ class MoodleApp(QMainWindow):
         webbrowser.open(url)
 
     def run_download(self):
+        def download_finished(fileno):
+            self.update_files_list()
+            if self.minimised:
+                self.tray_icon.showMessage(
+                    'xMoodle',
+                    f'{fileno} Files Downloaded',
+                )
+            self.download_running = False
+
+        def download_error(error):
+            self.download_running = False
+            if self.minimised:
+                self.tray_icon.showMessage(
+                    "xMoodle",
+                    error,
+                )
+            self.downloadLabel.setText(error)
+
         if self.download_running:
             return
         self.download_running = True
-        self.downloadLabel.setText('Logging In...')
-        self.update()
-        loop = asyncio.get_event_loop()
-        moodle = MoodleSession(self.config['urls']['home'], self.config['urls']['login'])
-
-        try:
-            loop.run_until_complete(moodle.login(self.config['logindata']))
-        except IncorrectLogindata:
-            self.downloadLabel.setText('Incorrect Logindata')
-            self.download_running = False
-            return
-        except ClientConnectionError:
-            self.downloadLabel.setText('No Internet Connection')
-            self.download_running = False
-            return
-        except TimeoutError:
-            self.downloadLabel.setText('Bad Network Connection')
-            self.download_running = False
-            return
-        except Exception as e:
-            self.downloadLabel.setText('Error')  # Log the error here
-            print(traceback.format_exc())
-            self.download_running = False
-            return
-
-        self.downloadLabel.setText('Gathering Course Content...')
-        self.update()
-
-        with open('courses.json', 'r') as courses_file:
-            all_courses = load(courses_file)
-
-        courses = []
-
-        for course in all_courses:
-            if course['checked']:
-                new_course = MoodleCourse.from_dict(course)
-                courses.append(new_course)
-
-        course_content = asyncio.gather(*[moodle.get_course_content(course) for course in courses])
-        loop.run_until_complete(course_content)
-
-        self.downloadLabel.setText('Comparing Files...')
-        self.update()
-
-        with open('files.json', 'r') as files_file:
-            downloaded_files = load(files_file)
-
-        downloaded_files_urls = [file['url'] for file in downloaded_files]
-
-        files_to_download = []
-
-        for course in courses:
-            for section in course.sections:
-                for file in section.files:
-                    if file.url not in downloaded_files_urls:
-                        file.path = f'{course.name}/{file.path}'
-                        files_to_download.append(file)
-
-        self.downloadLabel.setText(f'Downloading Files... ({len(files_to_download)} Files)')
-
-        file_download = [moodle.download_file(file, f"{self.config['default_path']}") for file in files_to_download]
-        downloaded_paths = loop.run_until_complete(asyncio.gather(*file_download))
-
-        loop.run_until_complete(moodle.close())
-
-        for i, file in enumerate(files_to_download):
-            file.download_path = downloaded_paths[i]
-
-        downloaded_files += [file.to_dict() for file in files_to_download]
-
-        with open('files.json', 'w') as files_file:
-            dump(downloaded_files, files_file)
-
-        self.downloadLabel.setText(f'{len(files_to_download)} Files Downloaded')
-
-        self.update_files_list()
-
-        if self.minimised:
-            self.tray_icon.showMessage(
-                "xMoodle",
-                f"{len(files_to_download)} Files Downloaded",
-            )
-
-        self.download_running = False
+        worker = DownloadWorker(self.config)
+        worker.signals.state.connect(lambda state: self.downloadLabel.setText(state))
+        worker.signals.finished.connect(download_finished)
+        self.threadpool.start(worker)
 
     def update_files_list(self):
         with open('files.json', 'r') as files_file:
-            files_to_show = load(files_file)[-50:]
+            files_to_show = load(files_file)[:50:-1]
 
         for file in files_to_show:
             item = QListWidgetItem(f"{file['path'].split('/', 1)[0]}: {file['name']}")
@@ -204,6 +144,7 @@ class Settings(QMainWindow):
     def __init__(self, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi('settings.ui', self)
+        self.setFixedSize(421, 394)
 
         self.config = config
 
@@ -314,6 +255,93 @@ class Settings(QMainWindow):
             item.course_dict = course
 
             self.coursesList.addItem(item)
+
+
+class DownloadWorker(QtCore.QRunnable):  # https://www.learnpyqt.com/tutorials/multithreading-pyqt-applications-qthreadpool/
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.signals = DownloadWorkerSignals()
+
+
+    @QtCore.pyqtSlot()
+    def run(self):
+        self.signals.state.emit('Logging In...')
+        loop = asyncio.new_event_loop()
+        moodle = MoodleSession(self.config['urls']['home'], self.config['urls']['login'], loop=loop)
+
+        try:
+            loop.run_until_complete(moodle.login(self.config['logindata']))
+        except IncorrectLogindata:
+            self.signals.error.emit('Incorrect Logindata')
+            return
+        except ClientConnectionError:
+            self.signals.error.emit('No Internet Connection')
+            return
+        except TimeoutError:
+            self.signals.error.emit('Bad Network Connection')
+            return
+        except Exception as e:
+            self.signals.error.emit('Error')  # Log the error here
+            print(traceback.format_exc())
+            return
+
+        self.signals.state.emit('Gathering Course Content...')
+
+        with open('courses.json', 'r') as courses_file:
+            all_courses = load(courses_file)
+
+        courses = []
+
+        for course in all_courses:
+            if course['checked']:
+                new_course = MoodleCourse.from_dict(course)
+                courses.append(new_course)
+
+        course_content = asyncio.gather(*[moodle.get_course_content(course) for course in courses], loop=loop)
+        loop.run_until_complete(course_content)
+
+        self.signals.state.emit('Comparing Files...')
+
+        with open('files.json', 'r') as files_file:
+            downloaded_files = load(files_file)
+
+        downloaded_files_urls = [file['url'] for file in downloaded_files]
+
+        files_to_download = []
+
+        for course in courses:
+            for section in course.sections:
+                for file in section.files:
+                    if file.url not in downloaded_files_urls:
+                        file.path = f'{course.name}/{file.path}'
+                        files_to_download.append(file)
+
+        self.signals.state.emit(f'Downloading Files... ({len(files_to_download)} Files)')
+
+        file_download = [moodle.download_file(file, f"{self.config['default_path']}") for file in files_to_download]
+        downloaded_paths = loop.run_until_complete(asyncio.gather(*file_download, loop=loop))
+
+        loop.run_until_complete(moodle.close())
+
+        for i, file in enumerate(files_to_download):
+            file.download_path = downloaded_paths[i]
+
+        downloaded_files += [file.to_dict() for file in files_to_download]
+
+        with open('files.json', 'w') as files_file:
+            dump(downloaded_files, files_file)
+
+        self.signals.state.emit(f'{len(files_to_download)} Files Downloaded')
+
+        self.signals.finished.emit(len(files_to_download))
+
+
+class DownloadWorkerSignals(QtCore.QObject):
+    state = QtCore.pyqtSignal(str)
+    finished = QtCore.pyqtSignal(int)
+    error = QtCore.pyqtSignal(str)
 
 
 if __name__ == '__main__':
